266ada2daf2f3760346260bf7deba39b977695a8
siwonpark <siwonsiwon80@hotmail.com>
2021-01-09 21:03:58 -0500

    test

--- nwhacks-app/api/api.py
+++ nwhacks-app/api/api.py
@@ -1,14 +1,22 @@
 import psycopg2 as db
 from flask import Flask
 from service.auth import auth as auth_blueprint
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, String, MetaData
 
 app = Flask(__name__)
 
 # con = db.connect(database="postgres", user="siwonpark", password = "", host="localhost", post="5432")
 
 app.register_blueprint(auth_blueprint)
+db_string = "postgresql://siwonpark:@localhost:5432/postgres"
 
+db = create_engine(db_string)
+# table = Table('hi', MetaData(db), Column('nice', String))
 
+# with db.connect() as conn:
+#     table.create()
+    
 @app.route('/')
 def index():
     return "This is an example app"
--- nwhacks-app/api/service/auth.py
+++ nwhacks-app/api/service/auth.py
@@ -1,13 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for, request
+from models.user import User
+
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
     return 'Login'
 
-@auth.route('/signup')
-def signup():
-    return 'Signup'
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first()
+
+    if user: 
+        return redirect(url_for('auth.login'))
+
 
 @auth.route('/logout')
 def logout():
