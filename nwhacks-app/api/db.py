d52d5909c9086e28fae0d1768fbebdefd2dd73f4
siwonpark <siwonsiwon80@hotmail.com>
2021-01-09 22:18:23 -0500

    added db session and signup logic

--- nwhacks-app/api/db.py
+++ nwhacks-app/api/db.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy import Table, Column, String, MetaData
+from sqlalchemy.orm import sessionmaker
 from .config import DATABASE_URI
 from .models import Base
 
 engine = create_engine(DATABASE_URI)
+
+Session = sessionmaker(bind=engine)
 
 def recreate_database():
     Base.metadata.drop_all(engine)
--- nwhacks-app/api/service/auth.py
+++ nwhacks-app/api/service/auth.py
@@ -1,6 +1,8 @@
 import hashlib
 from flask import Blueprint, redirect, url_for, request
+from werkzeug.security import generate_password_hash, check_password_hash
 from ..models import User
+from ..db import Session
 
 auth = Blueprint('auth', __name__)
 
@@ -10,18 +12,23 @@ def login():
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
+    session = Session()
+
     email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
-
-    user = User.query.filter_by(email=email).first()
+    user = session.query(User).filter_by(email=email).first()
 
     if user: 
         return redirect(url_for('auth.login'))
     
-    new_user = User(email=email, name=name, password=hashlib.sha256(password.encode))
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    session.add(new_user)
+    session.commit()
 
+    return redirect(url_for('auth.login'))
 
+    
 
 @auth.route('/logout')
 def logout():
