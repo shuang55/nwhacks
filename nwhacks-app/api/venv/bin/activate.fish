f93c7df820b48707d05dc9cce3e5cb69c9664040
siwonpark <siwonsiwon80@hotmail.com>
2021-01-09 19:46:35 -0500

    added example api and new gitignore

--- nwhacks-app/api/venv/bin/activate.fish
+++ nwhacks-app/api/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/siwonpark/Desktop/nwhacks/nwhacks-backend/venv"
+set -gx VIRTUAL_ENV "/Users/siwonpark/Desktop/nwhacks/nwhacks-app/api/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
