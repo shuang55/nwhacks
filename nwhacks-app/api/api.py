35701c208b5627e6aaa2734be42c5e9c48329ee5
huang <tsanyu.huang@mail.utoronto.ca>
2021-01-09 18:19:38 -0800

    Add some misc files

--- nwhacks-app/api/api.py
+++ nwhacks-app/api/api.py
@@ -1,13 +1,14 @@
 import psycopg2 as db
 from flask import Flask
 from service.auth import auth as auth_blueprint
+from service.expense_manager import expense as expense_blueprint
 
 app = Flask(__name__)
 
 # con = db.connect(database="postgres", user="siwonpark", password = "", host="localhost", post="5432")
 
 app.register_blueprint(auth_blueprint)
-
+app.register_blueprint(expense_blueprint)
 
 @app.route('/')
 def index():
