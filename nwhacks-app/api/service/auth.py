bb185fad3667af26f1c81efc0deec04b1b7297d1
siwonpark <siwonsiwon80@hotmail.com>
2021-01-09 22:34:52 -0500

    Implemented login

--- nwhacks-app/api/service/auth.py
+++ nwhacks-app/api/service/auth.py
@@ -1,14 +1,27 @@
 import hashlib
-from flask import Blueprint, redirect, url_for, request
+from flask import Blueprint, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from ..models import User
 from ..db import Session
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET'])
 def login():
     return 'Login'
+    
+@auth.route('/login', methods=['POST'])
+def login_post():
+    session = Session()
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = session.query(User).filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        return redirect(url_for('auth.login'))
+
+    return "Logged in successfully!"
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
