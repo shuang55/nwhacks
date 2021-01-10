0546536f38499b3f3c4d05a922726c274980b72c
siwonpark <siwonsiwon80@hotmail.com>
2021-01-09 20:28:27 -0500

    added Login/signup/logout paths

--- nwhacks-app/api/api.py
+++ nwhacks-app/api/api.py
@@ -1,9 +1,15 @@
-import time
+import psycopg2 as db
 from flask import Flask
+from auth.auth import auth as auth_blueprint
 
 app = Flask(__name__)
 
+# con = db.connect(database="postgres", user="siwonpark", password = "", host="localhost", post="5432")
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+app.register_blueprint(auth_blueprint)
+
+
+@app.route('/')
+def index():
+    return "This is an example app"
+
